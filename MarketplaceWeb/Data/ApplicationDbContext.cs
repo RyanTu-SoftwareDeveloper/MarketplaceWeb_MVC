8373cdee5c1bf4cea9e95d9f32e4d8f6064f57a8
RyanTu-SoftwareDeveloper <ryantu89.work@gmail.com>
2024-01-06 21:47:09 -0800

    Seed Category Table

--- MarketplaceWeb/Data/ApplicationDbContext.cs
+++ MarketplaceWeb/Data/ApplicationDbContext.cs
@@ -10,5 +10,16 @@ namespace MarketplaceWeb.Data
         }
         public DbSet<Category> Categories { get; set; }
 
+        //using ModelBuilder we can see data
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Python book", DisplayOrder = 1},
+                new Category { Id = 2, Name = "Java book", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "c# book", DisplayOrder = 3 }
+
+                );
+        }
+
     }
 }
--- MarketplaceWeb/Migrations/20240107053715_SeedCategoryTable.Designer.cs
+++ MarketplaceWeb/Migrations/20240107053715_SeedCategoryTable.Designer.cs
@@ -3,6 +3,7 @@ using MarketplaceWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MarketplaceWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240107053715_SeedCategoryTable")]
+    partial class SeedCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
