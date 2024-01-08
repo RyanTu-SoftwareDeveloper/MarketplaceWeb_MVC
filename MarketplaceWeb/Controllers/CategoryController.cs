ae147627cab3ccedad5029949254055f6c4cd602
RyanTu-SoftwareDeveloper <ryantu89.work@gmail.com>
2024-01-07 20:46:20 -0800

    get all categories

--- MarketplaceWeb/Controllers/CategoryController.cs
+++ MarketplaceWeb/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MarketplaceWeb.Data;
+using MarketplaceWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MarketplaceWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
