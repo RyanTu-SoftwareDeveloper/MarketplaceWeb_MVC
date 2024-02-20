602708e8837cbb1e7d3b2baaee899a611650e4f9
RyanTu-SoftwareDeveloper <ryantu89.work@gmail.com>
2024-02-19 20:38:08 -0800

    implement UnitOfWork

---
+++ MarketplaceWeb.DataAccess/Repository/IRepository/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using MarketplaceWeb.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketplaceWeb.DataAccess.Repository.IRepository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _db;
+        public ICategoryRepository Category{ get; private set; }
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+        }
+       
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
