f919ddf5990071da64a44326346ad57bf052fe14
RyanTu-SoftwareDeveloper <ryantu89.work@gmail.com>
2024-02-25 20:54:44 -0800

    add foreign key in EF core and seed data

--- MarketplaceWeb.DataAccess/Migrations/20240226045254_addForeignKeyForCategoryProductRelation.Designer.cs
+++ MarketplaceWeb.DataAccess/Migrations/20240226045254_addForeignKeyForCategoryProductRelation.Designer.cs
@@ -3,6 +3,7 @@ using MarketplaceWeb.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MarketplaceWeb.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240226045254_addForeignKeyForCategoryProductRelation")]
+    partial class addForeignKeyForCategoryProductRelation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
