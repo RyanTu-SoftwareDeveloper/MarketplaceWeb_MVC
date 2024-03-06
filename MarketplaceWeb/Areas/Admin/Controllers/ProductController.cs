dada45bb0c90fa8aa8424931e87289e9e9be5672
RyanTu-SoftwareDeveloper <ryantu89.work@gmail.com>
2024-03-05 22:37:47 -0800

    edit product link in DataTable and add API endpoint for delete product

--- MarketplaceWeb/Areas/Admin/Controllers/ProductController.cs
+++ MarketplaceWeb/Areas/Admin/Controllers/ProductController.cs
@@ -105,39 +105,36 @@ namespace MarketplaceWeb.Areas.Admin.Controllers
         }
 
 
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(productFromDb);
-        }
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeletePOST(int? id)
-        {
-            Product? obj = _unitOfWork.Product.Get(u => u.Id == id);
-            if (obj == null)
-            {
-                return NotFound();
-            }
-            _unitOfWork.Product.Remove(obj);
-            _unitOfWork.Save();
-            TempData["success"] = "Product deleted successfully";
-            return RedirectToAction("Index");
-        }
+  
         #region API CallS
         [HttpGet]
         public IActionResult GetAll()
         {
             List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
             return Json(new { data = objProductList });
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+            var productTobeDeleted = _unitOfWork.Product.Get(u => u.Id == id);
+            if (productTobeDeleted == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            var oldImagePath =
+                Path.Combine(_webHostEnvironment.WebRootPath, 
+                productTobeDeleted.ImageUrl.TrimStart('\\'));
+
+            _unitOfWork.Product.Remove(productTobeDeleted);
+            _unitOfWork.Save();
+
+            if (System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath);
+            }
+
+            return Json(new { success = true, message = "Delele Successful" });
         }
         #endregion
     }
